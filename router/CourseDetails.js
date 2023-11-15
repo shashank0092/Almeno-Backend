f4d1fb0f0fcaf8b20ba0fb184226471c563bd25b
SHASHANK <shashank7016204503@gmail.com>
2023-11-15 13:00:15 +0530

    Id Check in courseDetails API

--- router/CourseDetails.js
+++ router/CourseDetails.js
@@ -1,31 +1,37 @@
-const express=require("express")
-const courseinfo=require("../models/coursedetails")
-const router=express.Router()
+const express = require("express")
+const courseinfo = require("../models/coursedetails")
+const router = express.Router()
 const bodyParser = require('body-parser');
-const student=require("../middleware/Student")
+const student = require("../middleware/Student")
 
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
 
 
-router.get("/courseinfo",student,async(req,res)=>{
+router.get("/courseinfo", student, async (req, res) => {
 
-    const {id}=req.query
-   
-    try{
-        
-     
-        const coursedeatils=await courseinfo.find({"cid":id});
-      
-        if(!coursedeatils){
-            res.status(204).json({message:"Course Details Data Is Not Avliable Now"})
+    const { cid } = req.query
+    console.log(cid, "this is id")
+    try {
+
+        if (cid == undefined) {
+            return res.status(204).json({ message: "Please Provide Id of course" })
         }
-        else{
-            res.status(200).json({message:"Course Details",data:coursedeatils})
+
+        else {
+            const coursedeatils = await courseinfo.find({ "cid": cid });
+
+
+            if (!coursedeatils) {
+                res.status(204).json({ message: "Course Details Data Is Not Avliable Now" })
+            }
+            else {
+                res.status(200).json({ message: "Course Details", data: coursedeatils })
+            }
         }
     }
-    catch(err){
-        console.log("Error while fetching dummy data->",err)
+    catch (err) {
+        console.log("Error while fetching dummy data->", err)
     }
 })
 module.exports = router;
